9dcb448a7ff5ee400d8c72d6a2c2598778930e5b
Envoyofhell <103904453+Envoyofhell@users.noreply.github.com>
2025-03-12 14:30:59 -0400

    add init.js

---
+++ client/src/init.js
@@ -0,0 +1,12 @@
+// init.js
+window.onload = () => {
+  const isMobile =
+    /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  if (isMobile) {
+    alert(
+      'PTCG-sim is still in mobile development. Please use a desktop for full functionality.'
+    );
+  }
+};
