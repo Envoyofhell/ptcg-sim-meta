c801abb01057c082172f38a710744a3a8dce9d06
Envoyofhell <103904453+Envoyofhell@users.noreply.github.com>
2025-03-12 21:49:15 -0400

    Update load-import-data.js

--- client/src/initialization/load-import-data/load-import-data.js
+++ client/src/initialization/load-import-data/load-import-data.js
@@ -3,12 +3,27 @@ import { refreshBoardImages } from '../../setup/sizing/refresh-board.js';
 
 export function loadImportData() {
   const importDataJSON = document.getElementById('importDataJSON').textContent;
+
   if (importDataJSON && importDataJSON.trim() !== '') {
-    const importData = JSON.parse(importDataJSON);
-    let actions = importData.filter((obj) => !('version' in obj)); // Remove any objects containing version property
+    let importData;
+
+    try {
+      importData = JSON.parse(importDataJSON);
+    } catch (error) {
+      console.error("Failed to parse import data JSON:", error);
+      // Display an error message to users or handle appropriately
+      return; // Exit the function on JSON parse error
+    }
+
+    // Remove any objects containing version property
+    const actions = importData.filter((obj) => !('version' in obj));
+
+    // Process each action
     actions.forEach((data) => {
       acceptAction(data.user, data.action, data.parameters, true);
     });
+    
+    // Refresh board images
     refreshBoardImages();
   }
 }
