c1f34fb97294b5578d39b3709be7c4b5fd3a6f80
Envoyofhell <103904453+Envoyofhell@users.noreply.github.com>
2025-03-14 11:37:59 -0400

    Update resizer.js

--- client/src/initialization/document-event-listeners/table/resizer.js
+++ client/src/initialization/document-event-listeners/table/resizer.js
@@ -2,7 +2,11 @@ import {
   oppHandleMouseDown, 
   selfHandleMouseDown,
   oppContainerDocument 
-} from '../../../front-end.js';
+} from '../../../../front-end.js';
+
+console.log('Imported oppContainerDocument:', oppContainerDocument);
+console.log('Imported oppHandleMouseDown:', oppHandleMouseDown);
+console.log('Imported selfHandleMouseDown:', selfHandleMouseDown);
 
 export const initializeResizers = () => {
   const selfResizer = document.getElementById('selfResizer');
