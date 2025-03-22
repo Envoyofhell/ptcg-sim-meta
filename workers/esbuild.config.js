b9e0de2b8ef84c9b39ba31935e356213560ee3d3
Jason Holloway <jasonh1993@gmail.com>
2025-03-22 19:41:43 -0400

    Create esbuild.config.js

---
+++ workers/esbuild.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entryPoints: ['index.js'],
+    bundle: true,
+    platform: 'node',
+    target: ['es2020'],
+    outfile: 'dist/worker.js',
+    external: ['@neondatabase/serverless', 'itty-router']
+  };
